ad357c6484a4bef4fa056c44980c0e1fad9d91a9
rifatdv <m.rifathosen17@gmail.com>
2025-09-08 16:56:22 +0600

    08/09/25

--- form/form.php
+++ form/form.php
@@ -9,38 +9,79 @@
 </head>
 
 <body>
+    <?php
+        $fnameErr = $lnameErr = $compayErr=$addressErr=$addresErr=$emailErr = $reasoncontactErr = $servicesErr ="";
+        $fname = $lname =$company=$address=$addres= $email = $reasoncontact = $services = $offering = $position = "";
+
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if(empty($_POST["fname"])){
+                $fnameErr = "First Name required";
+            }
+            else{
+                $fname = test_input($_POST["fname"]);
+            }
+
+            if(empty($_POST["lname"])){
+                $lnameErr = "Last Name required";
+            }
+            else{
+                $lname = test_input($_POST["lname"]);
+            }
+
+            if(empty($_POST["company"])){
+                $companyErr = "Company required";
+            }
+            else{
+                $company = test_input($_POST["company"]);
+            }
+
+                if(empty($_POST["address"])){
+                $addressErr = "Address required";
+            }
+            else{
+                $address = test_input($_POST["address"]);
+            }
+
+                            if(empty($_POST["addres"])){
+                $addresErr = "Address required";
+            }
+            else{
+                $addres = test_input($_POST["address"]);
+            }
+
+
+        }
+
+        function test_input($data){
+            return htmlspecialchars(stripslashes(trim($data)));
+        }   
+    ?>
 
     <div class="wrapper">
 
-        <form method="GET">
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <h3 style="color: brown;">Donor information</h3>
             <div class="form-group">
-                <label for="name">First Name <span>*</span></label>
-                <input type="text" id="name" required>
+                <input type="text" id="fname" name="fname" value="<?php echo $fname; ?>" placeholder="Enter first name"><span><strong>*</strong><?php echo $fnameErr;?></span> <br> 
             </div>
             <div class="form-group">
-                <label for="name">Last Name<span>*</span></label>
-                <input type="text" id="name" required>
+                <input type="text" id="lname" name="lname" value="<?php echo $lname; ?>" placeholder="Enter first name"><span><strong>*</strong><?php echo $lnameErr;?></span> <br> 
             </div>
             <div class="form-group">
-                <label for="name">Company</label>
-                <input type="text" id="name" required>
+                <input type="text" id="name" name="company" value="<?php echo $company; ?>" placeholder="Enter first name"><span><strong>*</strong><?php echo $companyErr;?></span> <br> 
             </div>
             <div class="form-group">
-                <label for="name">Address1<span>*</span></label>
-                <input type="text" id="name" required>
+                <input type="text" id="name" name="address" value="<?php echo $address; ?>" placeholder="Enter first name"><span><strong>*</strong><?php echo $addressErr;?></span> <br> 
             </div>
             <div class="form-group">
-                <label for="name">Address 2<span>*</span></label>
-                <input type="text" id="name" required>
+                <input type="text" id="name" name="address" value="<?php echo $addres; ?>" placeholder="Enter first name"><span><strong>*</strong><?php echo $addresErr;?></span> <br> 
             </div>
             <div class="form-group">
-                <label for="name">City<span>*</span></label>
-                <input type="text" id="name" required>
+                <input type="text" id="name" >
             </div>
             <div class="form-group">
                 <label for="name">State<span>*</span></label>
-                <select id="state" required>
+                <select id="state" >
                     <option value="">Select State </option>
                     <option value="dhaka">Dhaka</option>
                     <option value="chittagong">Chittagong</option>
@@ -55,12 +96,12 @@
 
             <div class="form-group">
                 <label for="name">Zip Code<span>*</span></label>
-                <input type="text" id="name" required>
+                <input type="text" id="name" >
             </div>
 
             <div class="form-group">
                 <label for="name">Country<span>*</span></label>
-                <select id="state" required>
+                <select id="state" >
                     <option value="">Select State </option>
                     <option value="Bangladesh">Bangladesh</option>
                     <option value="Austrilia">Austrilia</option>
@@ -85,7 +126,7 @@
 
             <div class="form-group">
                 <label for="email">Email<span>*</span></label>
-                <input type="email" id="email" required>
+                <input type="email" id="email" >
 
             </div>
 
@@ -95,7 +136,7 @@
                     <p class="info">(check a button or type a amount)</p>
                     </div>
                 
-                <input type="radio" name="donation" required>None<br>
+                <input type="radio" name="donation" >None<br>
                 <input type="radio" name="donation">$50<br>
                 <input type="radio" name="donation">$75<br>
                 <input type="radio" name="donation">$100<br>
@@ -114,7 +155,7 @@
                 </div>
                 <div>
                     
-                    <input type="checkbox" value="App development" required>I am interested in giving on a regular
+                    <input type="checkbox" value="App development" >I am interested in giving on a regular
                     basis<br>
                     <label for="monthly_cc">Monthly Credit Card</label>
                     <input class="sizebox" type="text" id="name">
@@ -130,8 +171,8 @@
 
                 <label for="name">I would like to make this donation</label>
 
-                <input type="radio" value="none" required>To Honor<br>
-                <input type="radio" value="50" required>In Memory of<br>
+                <input type="radio" value="none" >To Honor<br>
+                <input type="radio" value="50" >In Memory of<br>
 
 
             </div>
@@ -142,7 +183,7 @@
             </div>
             <div class="form-group">
                 <label for="name">Acknowlegge Donation to<span>*</span></label>
-                <input type="text" id="name" required>
+                <input type="text" id="name" >
             </div>
             <div class="form-group">
                 <label for="name">Address</label>
@@ -155,7 +196,7 @@
             </div>
             <div class="form-group">
                 <label for="name">State<span>*</span></label>
-                <select id="state" required>
+                <select id="state" >
                     <option value="">Select State </option>
                     <option value="dhaka">Dhaka</option>
                     <option value="chittagong">Chittagong</option>
@@ -169,7 +210,7 @@
             </div>
             <div class="form-group">
                 <label for="name">Zip Code<span>*</span></label>
-                <input type="text" id="name" required>
+                <input type="text" id="name" >
             </div>
 
             <h3 style="color: brown;">Additional Information</h3>
@@ -184,7 +225,7 @@
 
                 <div>
 
-                    <input type="checkbox" name="donation" required>I would like to gift to remain anonymous.<br>
+                    <input type="checkbox" name="donation" >I would like to gift to remain anonymous.<br>
                     <input type="checkbox" name="donation">My empoyer offers a matching gift program. I will mail the
                     matching gift form.<br>
                     <input type="checkbox" name="donation" donation>Please save the cost of acknowledging this gift by
@@ -212,7 +253,7 @@
                 <label for="name">How may we contact you ?</label>
                 <div>
 
-                    <input type="checkbox" name="donation" required>E-mail<br>
+                    <input type="checkbox" name="donation" >E-mail<br>
                     <input type="checkbox" name="donation">Postal mail<br>
                     
                     <input type="checkbox" name="donation">Telephone<br>
@@ -226,13 +267,13 @@
                 <label for="name">How may we contact you ?</label>
                 <div>
 
-                    <input type="checkbox" name="donation" required>E-mail<br>
+                    <input type="checkbox" name="donation" >E-mail<br>
                     <input type="checkbox" name="donation">Postal mail<br>
 
                 </div>
             </div>
 
-            <input type="checkbox" value="App development" required>I would like infromation about volunteering with
+            <input type="checkbox" value="App development" >I would like infromation about volunteering with
             the<br><hr>
 
             <div class="form-group">
---
+++ xcompany/php/xcomhom.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>X Company</title>
+    <link rel="stylesheet" href="../css/xcomhom.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo">
+                <span class="x">X</span><span class="company">Company</span>
+            </div>
+            <div class="nav">
+                <a href="#">Home</a> |
+                <a href="#">Login</a> |
+                <a href="#">Registration</a>
+            </div>
+        </div>
+        <div class="main">
+            <b>Welcome to xCompany</b>
+        </div>
+        <div class="footer">
+            Copyright &copy; 2017
+        </div>
+    </div>
+</body>
+</html>
